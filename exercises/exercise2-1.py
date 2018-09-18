d7a4ad2d2b20c4611210993cb452e1ebfe008463
s-kganz <ganzkeenan1@gmail.com>
2018-09-17 20:12:11 -0400

    Added exercise 2-1

---
+++ exercises/exercise2-1.py
@@ -0,0 +1,29 @@
+"""
+Write a program that takes in an age from a user.
+Then, have the user put in an "in-between age", something like 13.5 (if one is 
+thirteen and a half) floating point. Which is a floating point integer
+(or a float) and which is just a regular integer?
+"""
+
+#TODO your code here
+
+"""
+Next, try adding the two numbers and place that values in a separate variable.
+Print that variable. Did it do what you expected it to do. What TYPE of number does it print?
+"""
+
+#TODO your code here
+
+"""
+How would you actually add the ages together? Change your program to show
+the two ages added together.
+"""
+
+#TODO your code here
+
+"""
+Extension: If you finish that, try adding a float with ".0" to an integer.
+Print this. Then, try adding two floats together. Print this. What data types printed out?
+"""
+
+#TODO your code here
