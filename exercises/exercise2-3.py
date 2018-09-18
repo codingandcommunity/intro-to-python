d4222052663f2e7121e0f87e352a60609e9026f6
s-kganz <ganzkeenan1@gmail.com>
2018-09-17 20:16:12 -0400

    added exercise2-3

---
+++ exercises/exercise2-3.py
@@ -0,0 +1,9 @@
+#And truth tables
+print("-------------------------")
+print("| P    " + "| Q    " + "| P AND Q |")
+print("-------------------------")
+statement1 = #insert True/False statement here
+statement2 = #insert True/False statement here
+print("|" + str(statement1) + "  |" + str(statement2) + "  |" +
+str(statement1 and statement2) + "     |")
+print("-------------------------")
