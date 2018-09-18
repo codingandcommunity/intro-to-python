896957426b825f0c22be759243bc8d7abd1c2c80
s-kganz <ganzkeenan1@gmail.com>
2018-09-17 20:28:03 -0400

    refine 2-3

--- Lesson 2/exercise2-3.py
+++ Lesson 2/exercise2-3.py
@@ -1,4 +1,9 @@
-#And truth tables
+'''
+
+An example of truth tables with AND
+Remove the quotes above and below to see what the program does.
+
+And truth tables
 print("-------------------------")
 print("| P    " + "| Q    " + "| P AND Q |")
 print("-------------------------")
@@ -7,3 +12,5 @@ statement2 = #insert True/False statement here
 print("|" + str(statement1) + "  |" + str(statement2) + "  |" +
 str(statement1 and statement2) + "     |")
 print("-------------------------")
+
+'''
