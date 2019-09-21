a2ab8d185f0f875d78c243740d366874bc4ca712
s-kganz <ganzkeenan1@gmail.com>
2019-09-21 10:06:26 -0400

    more hints in exercises

--- beginner/lesson2/practice.py
+++ beginner/lesson2/practice.py
@@ -8,7 +8,7 @@ Example output:
 What is your favorite drink? Water
 I love Water too!
 '''
-
+# Your code here
 
 '''
 Level 2: Create a program that asks the user for their age and a year.
@@ -21,10 +21,15 @@ How old are you? 19
 Give me a year: 2022
 You will be 22 years old in 2022.
 '''
+# Your code here
 
 '''
 Level 3: Create a program that asks the user for their name and then
          prints their name in a frame of stars.
+
+Hint: You can make 6 stars by typing "*" * 6
+      Get the length of a string with len()
+      example: len("Keenan") = 6
 
 Example output:
 ------------------------------------
@@ -33,3 +38,5 @@ What is your name? Keenan
 * Keenan *
 **********
 '''
+
+# Your code here
