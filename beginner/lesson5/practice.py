e3539ab0a2efef1eec7e4f9dcbb04dfa6a048687
neha-deshpande001 <37943727+neha-deshpande001@users.noreply.github.com>
2019-09-25 19:42:23 -0400

    Create practice.py

---
+++ beginner/lesson5/practice.py
@@ -0,0 +1,31 @@
+# Level 1
+# Add up all the numbers in an array. Print out the total.
+
+listOfNumbers = [1, 38, -28, 91, 193, 0]
+
+
+
+# Level 2
+# Given a list of students and a corresponding list of grades, print out the names of all students whose grade is 100.
+# In this example, the program should print
+# Ben
+# Carl
+
+listOfStudents = ["Abby", "Ben", "Carl", "Dave"]
+listOfGrades = [95, 100, 100, 92]
+
+
+
+# Level 3
+# Print out the sum of each column in a nested (2D) list
+# The output should be:
+# Sum of column 1: 6
+# Sum of column 2: 8
+# Sum of column 3: 21
+
+a = [ [ 2, 3, 5 ] ,
+      [ 1, 4, 7 ] ,
+      [ 3, 1, 9 ] ]
+
+
+
