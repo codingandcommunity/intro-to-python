d91f61fbda903936621f1bc3069ce202610b5398
Brandonkl <brandon.klein0910@gmail.com>
2019-09-25 19:27:20 -0400

    Starter code for guessing game

---
+++ beginner/lesson4/Project.py
@@ -0,0 +1,17 @@
+import random
+
+print("Guess the number that's in my mind")
+print("Hint: It's between 0 and 100")
+
+n = random.randint(1, 100) #pick a random number from 1 through 100
+#tries = ??
+#found = ??
+while True: 
+    #tries+=
+    p = int(input("Enter number -> "))
+    #if ??:
+        
+    #elif ??:
+
+    #else:
+        #print("You won in", tries, "tries!")
