2f1e406ed2cbb2fb6fe1b3e6ea3a161568bbe4b7
s-kganz <ganzkeenan1@gmail.com>
2019-10-07 18:18:08 -0400

    update loop project file

---
+++ beginner/lesson5/project.py
@@ -0,0 +1,18 @@
+import random
+
+print("Guess the number that's in my mind")
+print("Hint: It's between 0 and 100")
+
+n = random.randint(1, 100) #pick a random number from 1 through 100
+#tries = ??
+max_tries = 10
+#found = ??
+for i in range(???) 
+    #tries+=
+    p = int(input("Enter number -> "))
+    #if ??:
+        
+    #elif ??:
+
+    #else:
+        #print("You won in", tries, "tries!")
