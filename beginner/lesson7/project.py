f172fb9f15c73cc3546ee1969da59f42b844965e
splanco <43675748+splanco@users.noreply.github.com>
2019-10-09 18:50:04 -0400

    Update project.py

--- beginner/lesson7/project.py
+++ beginner/lesson7/project.py
@@ -1,1 +1,14 @@
-# Caesar Cypher
+'''
+Your task is to create a funcion that takes a string and turns it into a caesar cypher.
+If you do not know what a caesar cipher is, here's a link to a good description:
+https://learncryptography.com/classical-encryption/caesar-cipher
+'''
+
+def makeCipher(string, offset):
+    # Your code here
+    return string
+
+s = input("Enter a string to be turned into a cipher: ")
+o = input("Enter an offset for your cipher: ")
+cipher = makeCipher(s, o)
+print("You're new cipher is: %s".format(cipher))
